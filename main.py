c0f92b927c77160459a40dd5153a5e4b6ad6cc95
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-10-09 22:57:18 -0400

    small changes

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import sqlalchemy as create_engine
 import requests as rq
 
 # please note that each variable would be different for each mysql database and user
-MYSQL_HOSTNAME = '20.169.16.174'
-MYSQL_DATABASE = 'opiodTreatments'
-MYSQL_USER = 'lozoWeek6'
-MYSQL_PASSWORD = 'lozoAHI2023!'
 
-connect= f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
-engine = create_engine(connect)
+# MYSQL_HOSTNAME = '20.169.16.174'
+# MYSQL_DATABASE = 'opiodTreatments'
+# MYSQL_USER = 'lozoWeek6'
+# MYSQL_PASSWORD = 'lozoAHI2023!'
 
-print(engine.table_names())
+# connect= f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
+# engine = create_engine(connect)
+
+# print(engine.table_names())
 
 api = rq.get('https://data.cms.gov/data-api/v1/dataset/f1a8c197-b53d-4c24-9770-aea5d5a97dfb/data')
 api = api.json()
