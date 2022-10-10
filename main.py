8f17ad319577a51581daec76ef4946449cfe76d0
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-10-09 23:03:58 -0400

    small changes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import sqlalchemy as create_engine
 import requests as rq
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
 
 # please note that each variable would be different for each mysql database and user
-
-# MYSQL_HOSTNAME = '20.169.16.174'
-# MYSQL_DATABASE = 'opiodTreatments'
+MYSQL_HOSTNAME = '20.169.16.174'
+MYSQL_DATABASE = 'opiodTreatments'
 # MYSQL_USER = 'lozoWeek6'
 # MYSQL_PASSWORD = 'lozoAHI2023!'
 
-# connect= f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
-# engine = create_engine(connect)
+connect = f'mysql+pymysql://{config["MYSQL_USER"]}:{config["MYSQL_PASSWORD"]}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}' ## we can string together all the components to form the connection string
 
-# print(engine.table_names())
+engine = create_engine(connect) ## create_engine allows us to link the database via mysql
+
+print(engine.table_names())
 
 api = rq.get('https://data.cms.gov/data-api/v1/dataset/f1a8c197-b53d-4c24-9770-aea5d5a97dfb/data')
 api = api.json()
